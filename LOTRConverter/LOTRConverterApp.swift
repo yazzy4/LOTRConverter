4643c92dd2c0537590a0cb6c36a859bf8c928a29
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-10-12 13:39:11 -0400

    first commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Yaz Burrell on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
